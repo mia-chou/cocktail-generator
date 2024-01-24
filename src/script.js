69b156f5a444ec66794277c8af2a66995a8a16cc
mia-chou <mia.fy.chou@hotmail.com>
2024-01-24 14:45:20 +0000

    Added basic form heading and added Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
-alert("tim");
+function generate(event) {
+    event.preventDefault();
+    
+
+    let recipeElement=document.querySelector("#recipe");
+
+    new Typewriter("#recipe", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+    }
+    
+
+    recipeElement.innerHTML = `<span class="blink">⏳</span> Please wait a moment...`;
+
+
+   
+
+
+
+
+
+let recipeForm = document.querySelector("#recipe-generator");
+recipeForm.addEventListener("submit", generate);
