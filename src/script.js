78409a6294f867669c17e1fcc8738bf71396ea5b
mia-chou <mia.fy.chou@hotmail.com>
2024-01-25 14:35:20 +0000

    Fixed incorrect code

--- src/script.js
+++ src/script.js
@@ -1,34 +1,22 @@
 function displayRecipe(response) {
     console.log("recipe-generated");
-
-    new Typewriter("#recipe", {
-        strings: response.data.answer,
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-    });
-}
-
-function generate(event) {
-    event.preventDefault();
-    let instructionsInput = document.querySelector("#user-instructions");
-
-    let apiKey = "03ob1b49a93e50d82f3dta0c9ad62afc";
-
-    let context = `Please generate a recipe based on ${instructionsInput.value}`;
-
-    let prompt =
-        "You are a highly skilled AI Bartender that knows a range of different cocktail recipes from just one ingredient. Your mission is to generate a 6-line recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the recipe. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-
   
+    new Typewriter("#recipe", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
   
   function generateRecipe(event) {
     event.preventDefault();
   
-    let instructionsInput = document.querySelector("#user-instructions");
+    let instructionsInput = document.querySelector("#search-input");
     let apiKey = "03ob1b49a93e50d82f3dta0c9ad62afc";
-    let context = "You are a highly skilled AI Bartender that knows a range of different cocktail recipes from just one ingredient. Your mission is to generate a 6-line recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the recipe. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-
+    let context =
+      "You are a highly skilled AI Bartender that knows a range of different cocktail recipes from just one ingredient. Your mission is to generate a 6-line recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the recipe. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  
     let prompt = `User instructions: Generate a cocktail recipe about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
@@ -38,7 +26,5 @@ function generate(event) {
   
     axios.get(apiURL).then(displayRecipe);
   }
-  
-  let poemFormElement = document.querySelector("#recipe-generator-form");
-  poemFormElement.addEventListener("submit", generateRecipe);
-}
+  let recipeFormElement = document.querySelector("#recipe-generator");
+  recipeFormElement.addEventListener("submit", generateRecipe);
